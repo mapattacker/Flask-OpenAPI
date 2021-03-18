1e734b5297bb045902166ecaf9a6c1377a1162c6
Jake <mapattacker@gmail.com>
2021-03-18 16:00:26 +0800

    change to no api docs

--- server_http.py
+++ server_http.py
@@ -9,24 +9,24 @@ from pydantic import BaseModel, Field, confloat
 from predict import prediction
 
 app = Flask(__name__)
-api = FlaskPydanticSpec("flask", title="Objection Detection", version="v1.0.0")
+api = FlaskPydanticSpec()
 
 
 class RequestSchema(BaseModel):
-    maxResults: int = Field(None, example=20, description="Maximum detection result to return")
-    min_height: float = Field(None, example=0.3, description="Score")
-    min_width: float = Field(None, example=0.3, description="Score")
-    score_th: float = Field(None, example=0.3, description="Score")
-    nms_iou: float = Field(..., example=0.4, description="Non-max suppression, intersection over union")
-    type: str = Field(..., example="safetycone", description="name of object to detect")
-    image: str = Field(..., description="base64-encoded-image")
+    maxResults: int = 20
+    min_height: float = 0.3
+    min_width: float = 0.3
+    score_th: float = 0.3
+    nms_iou: float = 0.4
+    type: str "safetycone"
+    image: str
 
 class _normalizedVertices(BaseModel):
-    x: float = Field(..., example=5.12, description="X-coordinate")
-    y: float = Field(..., example=20.56, description="Y-coordinate")
-    width: int = Field(..., example=500, description="width in pixel")
-    height: int = Field(..., example=600, description="height in pixel")
-    score: confloat(gt=0.0, lt=1.0) = Field(..., example=0.79, description="confidence score")
+    x: float
+    y: float
+    width: int
+    height: int
+    score: confloat(gt=0.0, lt=1.0)
 
 class ResponseSchema(BaseModel):
     normalizedVertices: List[_normalizedVertices]
@@ -35,13 +35,9 @@ class ResponseSchema(BaseModel):
 @app.route("/predict", methods=["POST"])
 @api.validate(
     body=Request(RequestSchema),
-    resp=Response(HTTP_200=ResponseSchema),
-    tags=["API Name"]
+    resp=Response(HTTP_200=ResponseSchema)
 )
 def get_predictions():
-    """Short description of endpoint
-
-    Long description of endpoint"""
     JScontent = request.json
     img = JScontent["image"]
     response = prediction(img)
@@ -49,5 +45,4 @@ def get_predictions():
 
 
 if __name__ == "__main__":
-    api.register(app)
     app.run(host="0.0.0.0")
