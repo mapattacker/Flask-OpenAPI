1b6e4660325c7b9bd5d8f1e4347c0e761796b11b
Siyang <teo_siyang@imda.gov.sg>
2021-03-22 20:33:31 +0800

    Change to connexion

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Flask-OpenAPI
+
+Example of Flask with an OpenAPI ymal documentation using [connexion](https://github.com/zalando/connexion). The Swagger UI docs can be access from the endpoint `/ui`.
+
+See how to write an OpenAPI docs from the official [site](https://swagger.io/docs/specification/basic-structure/).
+
+## Other Examples
+
+ * request-response validation with [pydantic](https://pydantic-docs.helpmanual.io)
+    * branch **flask-pydantic-spec-nodocs**
+ * auto openapi docs generation (without yaml file), using [flask-pydantic-spec](https://github.com/turner-townsend/flask-pydantic-spec)
+    * branch **flask-pydantic-spec**
--- server_http.py
+++ server_http.py
@@ -1,47 +1,15 @@
-"""flask server with pydantic validation & openapi integration"""
+"""flask server with openapi.yml integration"""
 
-from typing import List
-
-from flask import Flask, request
-from flask_pydantic_spec import FlaskPydanticSpec, Request, Response
-from pydantic import BaseModel, Field, confloat
+import connexion
+from flask import request
 
 from predict import prediction
 
-app = Flask(__name__)
-api = FlaskPydanticSpec("flask", title="Objection Detection", version="v1.0.0")
-
-
-class RequestSchema(BaseModel):
-    maxResults: int = Field(None, example=20, description="Maximum detection result to return")
-    min_height: float = Field(None, example=0.3, description="Score")
-    min_width: float = Field(None, example=0.3, description="Score")
-    score_th: float = Field(None, example=0.3, description="Score")
-    nms_iou: float = Field(..., example=0.4, description="Non-max suppression, intersection over union")
-    type: str = Field(..., example="safetycone", description="name of object to detect")
-    image: str = Field(..., description="base64-encoded-image")
-
-class _normalizedVertices(BaseModel):
-    x: float = Field(..., example=5.12, description="X-coordinate")
-    y: float = Field(..., example=20.56, description="Y-coordinate")
-    width: int = Field(..., example=500, description="width in pixel")
-    height: int = Field(..., example=600, description="height in pixel")
-    score: confloat(gt=0.0, lt=1.0) = Field(..., example=0.79, description="confidence score")
-
-class ResponseSchema(BaseModel):
-    normalizedVertices: List[_normalizedVertices]
+app = connexion.App(__name__, specification_dir='.')
 
 
 @app.route("/predict", methods=["POST"])
-@api.validate(
-    body=Request(RequestSchema),
-    resp=Response(HTTP_200=ResponseSchema),
-    tags=["API Name"]
-)
-def get_predictions():
-    """Short description of endpoint
-
-    Long description of endpoint"""
+def predict():
     JScontent = request.json
     img = JScontent["image"]
     response = prediction(img)
@@ -49,5 +17,5 @@ def get_predictions():
 
 
 if __name__ == "__main__":
-    api.register(app)
+    app.add_api('openapi.yml')
     app.run(host="0.0.0.0")
